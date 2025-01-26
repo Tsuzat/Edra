6fa3ce8bfd37a42521ba2ec7609d933c834ed18c
Tsuzat <dev.tsuzat@gmail.com>
2025-01-26 10:17:40 +0530

    feat: Update Svelte configuration for adapter and prerendering options

--- svelte.config.js
+++ svelte.config.js
@@ -100,11 +100,22 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex(mdsvexConfigs)],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: true,
+			strict: false
+		}),
+		prerender: {
+			handleMissingId: 'ignore'
+		}
 	},
+	compilerOptions: {
+		css: 'injected',
+		enableSourcemap: true
+	},
+
 	extensions: ['.md', '.svx', '.svelte']
 };
 
