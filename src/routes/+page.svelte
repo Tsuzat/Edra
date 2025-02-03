28ad2e0aec2cd7e718e628bd2d9c3e10ad4c7a49
Tsuzat <dev.tsuzat@gmail.com>
2025-02-03 11:22:07 +0530

    refactor: Improve content storage and type safety in editor page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,31 @@
 	import Icons from '$lib/components/custom/icons/index.js';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import ShadEditor from '$lib/shad-editor/shad-editor.svelte';
-	import { writable } from 'svelte/store';
+	import { writable, type Writable } from 'svelte/store';
+	import defaultContent from '$lib/static/default_data.json' assert { type: 'json' };
+	import type { Content } from '@tiptap/core';
 
-	let localStorageContent = '';
-
+	let jsonContent: Content = {};
 	if (browser) {
-		localStorageContent =
-			localStorage.getItem('content') ||
-			`
-<h1 class="tiptap-heading">Creating a rich text editor with Tiptap, shadcn and ❤️</h1><p>This editor supports markdown out of the box. We have <strong>bold</strong>, <em>italic</em>, <s>strike</s>, <u>underline</u>, <a target="_blank" rel="noopener noreferrer" href="https://tsuzat.com">link</a>, <code>code</code>, Superscript A<sup>b</sup>, subscript A<sub>b</sub>, <mark data-color="#940080" style="background-color: #940080; color: inherit">highlight1</mark><mark data-color="#1a1600" style="background-color: #1a1600; color: inherit">,</mark> <mark data-color="#0000FF" style="background-color: #0000FF; color: inherit">highlight2</mark>,<span style="color: rgb(78, 211, 21)">textcolor1, </span><span style="color: rgb(128, 0, 128)">textcolor2.</span></p><blockquote><p>Block Quote and Code highlight with lowlight and colorscheme onelight and onedark</p></blockquote><h2 class="tiptap-heading">Code Block</h2><pre><code class="language-typescript">// Main function
-function main(){
-    console.log("Hello Developers");
-}</code></pre><h2 class="tiptap-heading">Lists</h2><ul class="list-disc"><li><p>Unordered List</p></li></ul><ol class="list-decimal"><li><p>Ordered List</p></li></ol><ul data-type="taskList"><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Task List</p></div></li></ul><p></p><h2 class="tiptap-heading" style="text-align: center">Align Center</h2><p>Smilipicker, type <code>:)</code> and it becoms 🙂 or 😉. Typography support e.g. type <code>!=</code> and it becomes ≠. Similarly (c) becomes ©, -&gt; becomes → and many more like 1×2, ½. We also have color visualizer. #FFF, #000, #FF00FF&nbsp;can be visualized.</p><h2 class="tiptap-heading">Table</h2><table style="width: 488px"><colgroup><col style="width: 155px"><col style="width: 167px"><col style="width: 166px"></colgroup><tbody><tr><th colspan="1" rowspan="1" colwidth="155"><p>Hello World</p></th><th colspan="1" rowspan="1" colwidth="167"><p>Hello World</p></th><th colspan="1" rowspan="1" colwidth="166"><ul data-type="taskList"><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Hello World</p></div></li></ul></th></tr><tr><td colspan="1" rowspan="1" colwidth="155"><p>Hello World</p></td><td colspan="1" rowspan="1" colwidth="167"><p>Hello World</p></td><td colspan="1" rowspan="1" colwidth="166"><p>Hello World</p></td></tr><tr><td colspan="1" rowspan="1" colwidth="155"><p></p></td><td colspan="1" rowspan="1" colwidth="167"><p></p></td><td colspan="1" rowspan="1" colwidth="166"><p></p></td></tr></tbody></table><h2 class="tiptap-heading">Image Support</h2><img src="https://placehold.co/800x400/6A00F5/white" title="" width="714" height="100%" align="center"><h3 class="tiptap-heading">Explore More…</h3>
-		`;
+		try {
+			let localStorageContent = localStorage.getItem('content');
+			if (!localStorageContent) {
+				localStorage.setItem('content', JSON.stringify(defaultContent));
+				jsonContent = defaultContent;
+			} else {
+				jsonContent = JSON.parse(localStorageContent);
+			}
+		} catch (e) {
+			console.error('Error on parsing the content', e);
+			jsonContent = defaultContent;
+		}
 	}
 
-	const content = writable(localStorageContent);
+	const content: Writable<Content> = writable(jsonContent);
 
 	content.subscribe((value) => {
-		console.log('Content Changed');
 		if (!browser) return;
-		localStorage.setItem('content', value);
+		localStorage.setItem('content', JSON.stringify(value));
 	});
 </script>
 
