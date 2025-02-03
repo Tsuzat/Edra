114fbcdbd296bf8a0544fccac37998ffeefdc64d
Tsuzat <dev.tsuzat@gmail.com>
2025-02-03 12:51:37 +0530

    feat: Create AnimatedBlob component for dynamic visual effects

---
+++ src/lib/components/custom/AnimatedBlob.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+
+	interface Props {
+		class?: string;
+	}
+
+	let { class: className = '' }: Props = $props();
+</script>
+
+<div
+	class={cn(
+		'animate-blob absolute size-72 rounded-full opacity-50 mix-blend-multiply blur-3xl',
+		className
+	)}
+></div>
