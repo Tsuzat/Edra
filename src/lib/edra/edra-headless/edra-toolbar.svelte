c345e38855ccebd8b42187432319806d503d709e
Tsuzat <dev.tsuzat@gmail.com>
2025-02-25 10:19:30 +0530

    Enhance toolbar buttons with shortcut display and separator

--- src/lib/edra/edra-headless/edra-toolbar.svelte
+++ src/lib/edra/edra-headless/edra-toolbar.svelte
@@ -15,13 +15,16 @@
 		{@const groups = commands[keys].commands}
 		{#each groups as group}
 			{@const Icon = icons[group.iconName]}
+			{@const shortcut = group.shortCuts ? ` (${group.shortCuts[0]})` : ''}
 			<button
 				class="edra-command-button"
 				class:active={editor.isActive(group.name)}
 				onclick={() => group.action(editor)}
+				title={`${group.label}${shortcut}`}
 			>
 				<Icon class="edra-toolbar-icon" />
 			</button>
 		{/each}
+		<span class="separator"></span>
 	{/each}
 </div>
