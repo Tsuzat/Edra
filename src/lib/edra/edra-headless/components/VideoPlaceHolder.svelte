3d07ac167d7a8f314d87736d600b0f09b5c3e82e
Tsuzat <dev.tsuzat@gmail.com>
2025-02-26 20:25:55 +0530

    Add VideoPlaceHolder component for Tiptap integration with video insertion

---
+++ src/lib/edra/edra-headless/components/VideoPlaceHolder.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { NodeViewProps } from '@tiptap/core';
+	import Video from 'lucide-svelte/icons/video';
+	import { NodeViewWrapper } from 'svelte-tiptap';
+	const {}: NodeViewProps = $props();
+</script>
+
+<NodeViewWrapper class="edra-media-placeholder-wrapper" contenteditable="false">
+	<span class="edra-media-placeholder-content">
+		<Video class="edra-media-placeholder-icon" />
+		<span class="edra-media-placeholder-text">Insert A Video</span>
+	</span>
+</NodeViewWrapper>
