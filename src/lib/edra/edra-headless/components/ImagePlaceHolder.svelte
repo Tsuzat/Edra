8ca9f980b93df1bcdc02803c65e3d93a13baa17f
Tsuzat <dev.tsuzat@gmail.com>
2025-02-26 19:50:38 +0530

    Add ImagePlaceholder component for Tiptap integration

---
+++ src/lib/edra/edra-headless/components/ImagePlaceHolder.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { NodeViewProps } from '@tiptap/core';
+	import Image from 'lucide-svelte/icons/image';
+	import { NodeViewWrapper } from 'svelte-tiptap';
+	const {}: NodeViewProps = $props();
+</script>
+
+<NodeViewWrapper class="edra-media-placeholder-wrapper" contenteditable="false">
+	<span class="edra-media-placeholder-content">
+		<Image class="edra-media-placeholder-icon" />
+		<span class="edra-media-placeholder-text">Insert An Image</span>
+	</span>
+</NodeViewWrapper>
