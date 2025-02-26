cdb3569777874b09cfbff64fd9283ed47bf46ad7
Tsuzat <dev.tsuzat@gmail.com>
2025-02-26 11:07:24 +0530

    Bind content to Edra component and implement localStorage management for content persistence

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { Edra } from '$lib/edra/index.js';
+	import type { Content } from '@tiptap/core';
+	import { onMount } from 'svelte';
+
+	let content = $state<Content>();
+
+	if (browser) {
+		let rawData: Content = JSON.parse(localStorage.getItem('edra-content') ?? '{}');
+		content = rawData;
+	}
+
+	$effect(() => {
+		console.log('Content = ', content);
+		if (content) {
+			localStorage.setItem('edra-content', JSON.stringify(content));
+		}
+	});
 </script>
 
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Edra />
+<Edra bind:content />
