b77f1c445fa60351df2a2ac733f701f94374c7e1
Tsuzat <dev.tsuzat@gmail.com>
2025-02-26 19:50:45 +0530

    Add AudioPlaceHolder component for Tiptap integration

---
+++ src/lib/edra/edra-headless/components/AudioPlaceHolder.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { NodeViewProps } from '@tiptap/core';
+	import AudioLines from 'lucide-svelte/icons/audio-lines';
+	import { NodeViewWrapper } from 'svelte-tiptap';
+	const {}: NodeViewProps = $props();
+</script>
+
+<NodeViewWrapper class="edra-media-placeholder-wrapper" contenteditable="false">
+	<span class="edra-media-placeholder-content">
+		<AudioLines class="edra-media-placeholder-icon" />
+		<span class="edra-media-placeholder-text">Insert An Audio</span>
+	</span>
+</NodeViewWrapper>
