4a99cb0bced18f4e493d58ca449bfeb9f33b9382
Tsuzat <dev.tsuzat@gmail.com>
2025-03-04 11:05:34 +0530

    Enhance navbar with theme toggler and update layout for improved accessibility

--- src/lib/components/custom/navbar.svelte
+++ src/lib/components/custom/navbar.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+	import Coffee from 'lucide-svelte/icons/coffee';
+	import GitHub from 'lucide-svelte/icons/github';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import ThemeToggler from './theme-toggler.svelte';
 </script>
 
-<nav class="">
+<nav class="inline-flex w-full items-center justify-between gap-4 border-b border-border px-4 py-2">
+	<div class="inline-flex items-center gap-2">
+		<img src="/favicon.png" alt="logo" class="size-8" />
+		<span class="text-xl font-bold">Edra</span>
+	</div>
 	<div>
-		<img src="/favicon.png" alt="logo" class="logo" />
-		<span>E D R A</span>
+		<Button variant="link" href="/docs">Docs</Button>
+		<Button variant="outline" size="icon" href="https://buymeacoffee.com/tsuzat" target="_blank">
+			<Coffee />
+		</Button>
+		<Button variant="outline" size="icon" href="https://github.com/Tsuzat/Edra" target="_blank">
+			<GitHub />
+		</Button>
+		<ThemeToggler />
 	</div>
 </nav>
