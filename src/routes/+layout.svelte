1af0e4dbf13a95c80f5e66d9c1731998289f0233
Tsuzat <dev.tsuzat@gmail.com>
2025-03-04 11:07:09 +0530

    Add Navbar component to layout and include head tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import '@fontsource/poppins';
+	import Navbar from '$lib/components/custom/navbar.svelte';
 	// import '../app.css';
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
 
+<svelte:head></svelte:head>
+
+<Navbar />
 {@render children()}
