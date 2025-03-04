01ad5aaa9f5aad6f1e9df3a5ff52b0ca4b00f65e
Tsuzat <dev.tsuzat@gmail.com>
2025-03-04 11:05:31 +0530

    Add theme toggler component for light/dark mode switching

---
+++ src/lib/components/custom/theme-toggler.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon-star';
+	import { cn } from '$lib/utils.js';
+	import { mode, toggleMode } from 'mode-watcher';
+</script>
+
+<Button variant="outline" size="icon" class="theme-toggler" onclick={toggleMode}>
+	<Sun class={cn($mode === 'dark' ? 'hidden' : '')} />
+	<Moon class={cn($mode === 'light' ? 'hidden' : '')} />
+	<span class="sr-only">Toggle theme</span>
+</Button>
