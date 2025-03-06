e3d9343fb31930d3a6351ee4373e2dc7df7268b9
Tsuzat <dev.tsuzat@gmail.com>
2025-03-06 10:45:00 +0530

    Refactor toolbar to use EditorTool component and enhance color command functionality

--- src/lib/edra/edra-headless/edra-toolbar.svelte
+++ src/lib/edra/edra-headless/edra-toolbar.svelte
@@ -2,29 +2,72 @@
 	import type { Editor } from '@tiptap/core';
 	import { commands } from '../commands/commands.js';
 	import { icons } from 'lucide-svelte';
+	import type { EdraCommand } from '../commands/types.js';
 
 	interface Props {
 		editor: Editor;
 	}
 
 	let { editor }: Props = $props();
+
+	const colorCommands = commands.colors.commands;
 </script>
 
+{#snippet EditorTool(group: EdraCommand, style?: string, onclick?: () => void)}
+	{@const Icon = icons[group.iconName]}
+	{@const shortcut = group.shortCuts ? ` (${group.shortCuts[0]})` : ''}
+	<button
+		class="edra-command-button"
+		class:active={editor.isActive(group.name) || group.isActive?.(editor)}
+		onclick={() => {
+			if (onclick !== undefined) onclick();
+			else group.action(editor);
+		}}
+		title={`${group.label}${shortcut}`}
+		{style}
+	>
+		<Icon class="edra-toolbar-icon" />
+	</button>
+{/snippet}
+
 <div class="edra-toolbar">
-	{#each Object.keys(commands) as keys}
+	{#each Object.keys(commands).filter((key) => key !== 'colors') as keys}
 		{@const groups = commands[keys].commands}
 		{#each groups as group}
-			{@const Icon = icons[group.iconName]}
-			{@const shortcut = group.shortCuts ? ` (${group.shortCuts[0]})` : ''}
-			<button
-				class="edra-command-button"
-				class:active={editor.isActive(group.name) || group.isActive?.(editor)}
-				onclick={() => group.action(editor)}
-				title={`${group.label}${shortcut}`}
-			>
-				<Icon class="edra-toolbar-icon" />
-			</button>
+			{@render EditorTool(group)}
 		{/each}
 		<span class="separator"></span>
 	{/each}
+
+	{@render EditorTool(
+		colorCommands[0],
+		`color: ${editor.getAttributes('textStyle').color};`,
+		() => {
+			const color = editor.getAttributes('textStyle').color;
+			const hasColor = editor.isActive('textStyle', { color });
+			if (hasColor) {
+				editor.chain().focus().unsetColor().run();
+			} else {
+				const color = prompt('Enter the color of the text:');
+				if (color !== null) {
+					editor.chain().focus().setColor(color).run();
+				}
+			}
+		}
+	)}
+	{@render EditorTool(
+		colorCommands[1],
+		`background-color: ${editor.getAttributes('highlight').color};`,
+		() => {
+			const hasHightlight = editor.isActive('highlight');
+			if (hasHightlight) {
+				editor.chain().focus().unsetHighlight().run();
+			} else {
+				const color = prompt('Enter the color of the highlight:');
+				if (color !== null) {
+					editor.chain().focus().setHighlight({ color }).run();
+				}
+			}
+		}
+	)}
 </div>
