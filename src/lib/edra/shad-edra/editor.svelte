02702553d71ae82f8b1667c26d688a6d337cf3c3
Tsuzat <dev.tsuzat@gmail.com>
2025-03-07 16:16:01 +0530

    Implement editor component with Tiptap integration and custom toolbar

--- src/lib/edra/shad-edra/editor.svelte
+++ src/lib/edra/shad-edra/editor.svelte
@@ -1,2 +1,74 @@
 <script lang="ts">
+	import type { Content, Editor, Extensions } from '@tiptap/core';
+	import { onMount } from 'svelte';
+	import { initiateEditor } from '../editor.js';
+	import { Transaction } from '@tiptap/pm/state';
+	import Toolbar from './toolbar.svelte';
+
+	import '../editor.css';
+	import './style.css';
+	import '../onedark.css';
+
+	import 'katex/dist/katex.min.css';
+	import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
+	import CodeExtended from './components/CodeExtended.svelte';
+	import { all, createLowlight } from 'lowlight';
+	import { SvelteNodeViewRenderer } from 'svelte-tiptap';
+	const lowlight = createLowlight(all);
+
+	interface Props {
+		class?: string;
+		content?: Content;
+		editable?: boolean;
+		editor?: Editor;
+		/**
+		 * Callback function to be called when the content is updated
+		 * @param content
+		 */
+		onUpdate?: (props: { editor: Editor; transaction: Transaction }) => void;
+	}
+
+	let {
+		class: className = '',
+		content = $bindable(),
+		editable = true,
+		editor = $bindable<Editor | undefined>(),
+		onUpdate
+	}: Props = $props();
+
+	let element = $state<HTMLElement>();
+
+	onMount(() => {
+		editor = initiateEditor(
+			element,
+			content,
+			[
+				CodeBlockLowlight.configure({
+					lowlight
+				}).extend({
+					addNodeView() {
+						return SvelteNodeViewRenderer(CodeExtended);
+					}
+				})
+			],
+			{
+				editable,
+				onUpdate,
+				onTransaction(props) {
+					editor = undefined;
+					editor = props.editor;
+				}
+			}
+		);
+	});
 </script>
+
+<div class={`edra ${className} overflow-auto`}>
+	{#if editor}
+		<Toolbar {editor} />
+	{/if}
+	<div
+		bind:this={element}
+		class="edra-editor prose mx-8 my-4 max-w-full dark:prose-invert *:outline-none"
+	></div>
+</div>
