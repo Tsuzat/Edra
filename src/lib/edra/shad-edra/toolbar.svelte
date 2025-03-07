afefecc3187d40fbcadba538e3d3fe626d2636c5
Tsuzat <dev.tsuzat@gmail.com>
2025-03-07 16:16:10 +0530

    Add toolbar component for Tiptap integration with dynamic command rendering

---
+++ src/lib/edra/shad-edra/toolbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Editor } from '@tiptap/core';
+	import { commands } from '../commands/commands.js';
+	import EdraToolBarIcon from './components/EdraToolBarIcon.svelte';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+
+	interface Props {
+		editor: Editor;
+	}
+
+	const { editor }: Props = $props();
+</script>
+
+<div class="edra-toolbar flex h-fit w-fit items-center gap-1 overflow-auto">
+	{#each Object.keys(commands).filter((key) => key !== 'colors' && key !== 'fonts') as keys}
+		{@const groups = commands[keys].commands}
+		{#each groups as command}
+			<EdraToolBarIcon {command} {editor} />
+		{/each}
+	{/each}
+</div>
