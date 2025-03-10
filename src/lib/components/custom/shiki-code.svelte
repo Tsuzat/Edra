7a2ba95192a2219fce21199689c2bf186353a8a9
Tsuzat <dev.tsuzat@gmail.com>
2025-03-10 17:26:39 +0530

    Add custom Shiki code component for enhanced syntax highlighting

---
+++ src/lib/components/custom/shiki-code.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+	import {
+		codeToHtml,
+		type BundledLanguage,
+		type SpecialLanguage,
+		type StringLiteralUnion
+	} from 'shiki';
+	import { onMount } from 'svelte';
+
+	interface Props {
+		class?: string;
+		code: string;
+		lang: StringLiteralUnion<BundledLanguage | SpecialLanguage, string>;
+	}
+
+	const { class: className = '', code, lang }: Props = $props();
+
+	let html = $state('');
+	onMount(async () => {
+		html = await codeToHtml(code, {
+			lang,
+			themes: {
+				light: 'github-light-high-contrast',
+				dark: 'github-dark-high-contrast'
+			}
+		});
+	});
+</script>
+
+<div class={cn('relative rounded border border-border/50 p-4', className)}>
+	{@html html}
+</div>
