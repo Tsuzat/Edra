2ef0e1219376ed3ba3bf5c2b132bc4026417c26d
Tsuzat <dev.tsuzat@gmail.com>
2025-03-10 17:41:34 +0530

    Add slide transition effect for page content rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import Navbar from '$lib/components/custom/navbar.svelte';
 	import '../app.css';
 	import Footer from '$lib/components/custom/footer.svelte';
+	import { page } from '$app/state';
+	import { slide } from 'svelte/transition';
 	let { children } = $props();
 </script>
 
@@ -12,8 +14,14 @@
 
 <div class="relative flex min-h-screen flex-col">
 	<Navbar />
-	<div class="size-full flex-1">
-		{@render children()}
-	</div>
+	{#key page.url.href}
+		<div
+			in:slide={{ duration: 300, delay: 300, axis: 'x' }}
+			out:slide={{ duration: 300 }}
+			class="animated-load size-full flex-1"
+		>
+			{@render children()}
+		</div>
+	{/key}
 	<Footer />
 </div>
