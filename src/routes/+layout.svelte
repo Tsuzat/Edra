83b6c78fb410bd7230feb25c01115c5fd74246be
Tsuzat <dev.tsuzat@gmail.com>
2025-03-10 12:54:46 +0530

    Add footer component to layout with transition effects

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
-	import '@fontsource/poppins';
 	import Navbar from '$lib/components/custom/navbar.svelte';
 	import '../app.css';
+	import Footer from '$lib/components/custom/footer.svelte';
+	import { page } from '$app/state';
+	import { fade } from 'svelte/transition';
 	let { children } = $props();
 </script>
 
@@ -10,7 +12,14 @@
 
 <svelte:head></svelte:head>
 
-<div class="flex h-screen w-screen flex-col">
-	<Navbar />
-	{@render children()}
-</div>
+{#key page.url}
+	<div
+		in:fade={{ duration: 300, delay: 400 }}
+		out:fade={{ duration: 300 }}
+		class="relative flex min-h-screen flex-col"
+	>
+		<Navbar />
+		{@render children()}
+		<Footer />
+	</div>
+{/key}
