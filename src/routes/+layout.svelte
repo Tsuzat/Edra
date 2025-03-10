1f5cc95abd63990f364369a86bbe4935930c9ac2
Tsuzat <dev.tsuzat@gmail.com>
2025-03-10 16:14:17 +0530

    Refactor layout structure to simplify rendering and improve component organization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,6 @@
 	import Navbar from '$lib/components/custom/navbar.svelte';
 	import '../app.css';
 	import Footer from '$lib/components/custom/footer.svelte';
-	import { page } from '$app/state';
-	import { fade } from 'svelte/transition';
 	let { children } = $props();
 </script>
 
@@ -12,14 +10,10 @@
 
 <svelte:head></svelte:head>
 
-{#key page.url}
-	<div
-		in:fade={{ duration: 300, delay: 400 }}
-		out:fade={{ duration: 300 }}
-		class="relative flex min-h-screen flex-col"
-	>
-		<Navbar />
+<div class="relative flex min-h-screen flex-col">
+	<Navbar />
+	<div class="size-full flex-1">
 		{@render children()}
-		<Footer />
 	</div>
-{/key}
+	<Footer />
+</div>
