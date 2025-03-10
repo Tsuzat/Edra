4be1a23e790f00fa50c7901c755bc69066fc4e0b
Tsuzat <dev.tsuzat@gmail.com>
2025-03-10 12:54:40 +0530

    Add error page with navigation prompt for better user experience

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<div class="flex size-full flex-1 flex-col items-center justify-center">
+	Looks like you're lost in space. Try going back to the <a class="text-center underline" href="/"
+		>home page</a
+	>
+</div>
