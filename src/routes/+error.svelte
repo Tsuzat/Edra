20b4e107ffb9f4aaead64eef77556bf2377857ff
Tsuzat <dev.tsuzat@gmail.com>
2025-03-14 22:05:11 +0530

    Enhance 404 error page layout with improved styling and messaging

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,5 @@
-<div class="flex size-full flex-1 flex-col items-center justify-center">
-	Looks like you're lost in space. Try going back to the <a class="text-center underline" href="/"
-		>home page</a
-	>
+<div class="flex h-80 flex-grow flex-col items-center justify-center gap-8">
+	<h1 class="animate-bounce text-5xl font-extrabold">404 Not Found</h1>
+	<div>Looks like you're lost in space. Try going back to the</div>
+	<a class="text-center underline" href="/">home page</a>
 </div>
