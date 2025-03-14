b75c9edee6b427c49e1d2b504f4da8b907c9ccb6
Tsuzat <dev.tsuzat@gmail.com>
2025-03-14 22:55:23 +0530

    Add search and replace functionality to toolbar; integrate component and manage visibility state

--- src/lib/edra/edra-headless/toolbar.svelte
+++ src/lib/edra/edra-headless/toolbar.svelte
@@ -2,63 +2,69 @@
 	import type { Editor } from '@tiptap/core';
 	import { commands } from '../commands/commands.js';
 	import EdraToolBarIcon from './components/EdraToolBarIcon.svelte';
+	import SearcnAndReplace from './components/SearcnAndReplace.svelte';
 
 	interface Props {
 		editor: Editor;
 	}
 
-	let { editor }: Props = $props();
+	const { editor }: Props = $props();
+
+	let showSearchAndReplace = $state(false);
 
 	const colorCommands = commands.colors.commands;
 	const fontCommands = commands.fonts.commands;
 </script>
 
 <div class="edra-toolbar">
-	{#each Object.keys(commands).filter((key) => key !== 'colors' && key !== 'fonts') as keys}
-		{@const groups = commands[keys].commands}
-		{#each groups as command}
-			<EdraToolBarIcon {command} {editor} />
+	{#if !showSearchAndReplace}
+		{#each Object.keys(commands).filter((key) => key !== 'colors' && key !== 'fonts') as keys}
+			{@const groups = commands[keys].commands}
+			{#each groups as command}
+				<EdraToolBarIcon {command} {editor} />
+			{/each}
+			<span class="separator"></span>
 		{/each}
+
+		<EdraToolBarIcon command={fontCommands[0]} {editor} />
+		<span>{editor.getAttributes('textStyle').fontSize ?? '16px'}</span>
+		<EdraToolBarIcon command={fontCommands[1]} {editor} />
+
 		<span class="separator"></span>
-	{/each}
 
-	<EdraToolBarIcon command={fontCommands[0]} {editor} />
-	<span>{editor.getAttributes('textStyle').fontSize ?? '16px'}</span>
-	<EdraToolBarIcon command={fontCommands[1]} {editor} />
-
-	<span class="separator"></span>
-
-	<EdraToolBarIcon
-		command={colorCommands[0]}
-		{editor}
-		style={`color: ${editor.getAttributes('textStyle').color};`}
-		onclick={() => {
-			const color = editor.getAttributes('textStyle').color;
-			const hasColor = editor.isActive('textStyle', { color });
-			if (hasColor) {
-				editor.chain().focus().unsetColor().run();
-			} else {
-				const color = prompt('Enter the color of the text:');
-				if (color !== null) {
-					editor.chain().focus().setColor(color).run();
+		<EdraToolBarIcon
+			command={colorCommands[0]}
+			{editor}
+			style={`color: ${editor.getAttributes('textStyle').color};`}
+			onclick={() => {
+				const color = editor.getAttributes('textStyle').color;
+				const hasColor = editor.isActive('textStyle', { color });
+				if (hasColor) {
+					editor.chain().focus().unsetColor().run();
+				} else {
+					const color = prompt('Enter the color of the text:');
+					if (color !== null) {
+						editor.chain().focus().setColor(color).run();
+					}
 				}
-			}
-		}}
-	/>
-	<EdraToolBarIcon
-		command={colorCommands[1]}
-		{editor}
-		style={`background-color: ${editor.getAttributes('highlight').color};`}
-		onclick={() => {
-			const hasHightlight = editor.isActive('highlight');
-			if (hasHightlight) {
-				editor.chain().focus().unsetHighlight().run();
-			} else {
-				const color = prompt('Enter the color of the highlight:');
-				if (color !== null) {
-					editor.chain().focus().setHighlight({ color }).run();
+			}}
+		/>
+		<EdraToolBarIcon
+			command={colorCommands[1]}
+			{editor}
+			style={`background-color: ${editor.getAttributes('highlight').color};`}
+			onclick={() => {
+				const hasHightlight = editor.isActive('highlight');
+				if (hasHightlight) {
+					editor.chain().focus().unsetHighlight().run();
+				} else {
+					const color = prompt('Enter the color of the highlight:');
+					if (color !== null) {
+						editor.chain().focus().setHighlight({ color }).run();
+					}
 				}
-			}
-		}}
-	/>
+			}}
+		/>
+	{/if}
+	<SearcnAndReplace {editor} bind:show={showSearchAndReplace} />
 </div>
