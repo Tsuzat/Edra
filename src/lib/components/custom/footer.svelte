568cb4a5b68dd2df2f87cb5dff07b81d84e0fc87
Tsuzat <dev.tsuzat@gmail.com>
2025-03-16 14:53:52 +0530

    Refactor footer component for improved layout and updated copyright information

--- src/lib/components/custom/footer.svelte
+++ src/lib/components/custom/footer.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
-	import Button from '../ui/button/button.svelte';
-	import Coffee from 'lucide-svelte/icons/coffee';
 </script>
 
-<footer
-	class="bottom-0 z-50 flex h-fit flex-col items-center justify-center gap-2 p-2 sm:flex-row sm:p-4"
->
-	<span>
-		Made with ❤️ by <a
-			href="https://tsuzat.com"
-			class="mx-2 text-blue-600 underline"
-			target="_blank"
-			rel="noopener noreferrer">Tsuzat</a
-		>
+<footer class="mt-2 flex h-fit items-center justify-center gap-2 p-1 text-sm">
+	<a href="/docs" class="underline">Docs</a> •
+	<a href="https://github.com/Tsuzat/Edra" target="_blank" class="underline">Github</a> •
+	<span
+		>Copyright © 2025
+		<a href="https://tsuzat.com" target="_blank" class="underline">Tsuzat</a>
 	</span>
-	<Button variant="outline" href="https://buymeacoffee.com/tsuzat" target="_blank">
-		<Coffee /> Buy me a coffee
-	</Button>
-	<img src="https://waka-api.dev-tsuzat.workers.dev/Edra" alt="Waka Api Badge" />
+	•
+	<a href="/LICENSE" class="underline">License</a>
 </footer>
