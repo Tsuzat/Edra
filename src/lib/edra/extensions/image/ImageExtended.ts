560c689a722781bad2063c30985c60447eedad2f
Tsuzat <dev.tsuzat@gmail.com>
2025-03-16 13:21:02 +0530

    Enable Base64 image support in ImageExtended component

--- src/lib/edra/extensions/image/ImageExtended.ts
+++ src/lib/edra/extensions/image/ImageExtended.ts
@@ -30,5 +30,7 @@ export const ImageExtended = (component: Component<NodeViewProps>): Node<ImageOp
 		addNodeView: () => {
 			return SvelteNodeViewRenderer(component);
 		}
+	}).configure({
+		allowBase64: true
 	});
 };
