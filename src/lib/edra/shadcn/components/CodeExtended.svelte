c536402da43b8640e592f440eb5e7054b5efa5bb
Tsuzat <dev.tsuzat@gmail.com>
2025-03-18 10:09:09 +0530

    refactor: simplify CodeExtended component structure and improve styling

--- src/lib/edra/shadcn/components/CodeExtended.svelte
+++ src/lib/edra/shadcn/components/CodeExtended.svelte
@@ -30,49 +30,46 @@
 	draggable={false}
 	spellcheck={false}
 >
-	<DropdownMenu.Root>
-		<DropdownMenu.Trigger
-			contenteditable="false"
-			class={buttonVariants({
-				variant: 'ghost',
-				size: 'sm',
-				class:
-					'absolute left-2 top-2 h-4 rounded px-1 py-2 text-xs text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100'
-			})}
-			>{defaultLanguage}
-			<ChevronDown class="!size-3" />
-		</DropdownMenu.Trigger>
-		<DropdownMenu.Content class="h-96 w-40 overflow-auto" contenteditable="false">
-			{#each languages as language}
-				<DropdownMenu.Item
-					contenteditable="false"
-					data-current={defaultLanguage === language}
-					class="data-[current=true]:bg-muted"
-					textValue={language}
-					onclick={() => {
-						defaultLanguage = language;
-						updateAttributes({ language: defaultLanguage });
-					}}
-				>
-					<span>{language}</span>
-					{#if defaultLanguage === language}
-						<Check class="ml-auto" />
-					{/if}
-				</DropdownMenu.Item>
-			{/each}
-		</DropdownMenu.Content>
-	</DropdownMenu.Root>
-	<Button
-		variant="ghost"
-		class="absolute right-2 top-2 size-4 p-0 text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100"
-		onclick={copyCode}
-	>
-		{#if isCopying}
-			<Check class="size-3 text-green-500" />
-		{:else}
-			<Copy class="size-3" />
-		{/if}
-	</Button>
+	<div class="code-wrapper-tile" contenteditable="false">
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger
+				contenteditable="false"
+				class={buttonVariants({
+					variant: 'ghost',
+					size: 'sm',
+					class: 'h-4 rounded px-1 py-2 text-xs text-muted-foreground'
+				})}
+				>{defaultLanguage}
+				<ChevronDown class="!size-2" />
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content class="h-96 w-40 overflow-auto" contenteditable="false">
+				{#each languages as language}
+					<DropdownMenu.Item
+						contenteditable="false"
+						data-current={defaultLanguage === language}
+						class="data-[current=true]:bg-muted"
+						textValue={language}
+						onclick={() => {
+							defaultLanguage = language;
+							updateAttributes({ language: defaultLanguage });
+						}}
+					>
+						<span>{language}</span>
+						{#if defaultLanguage === language}
+							<Check class="ml-auto" />
+						{/if}
+					</DropdownMenu.Item>
+				{/each}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+		<Button variant="ghost" class="size-4 p-0 text-muted-foreground" onclick={copyCode}>
+			{#if isCopying}
+				<Check class="size-3 text-green-500" />
+			{:else}
+				<Copy class="size-3" />
+			{/if}
+		</Button>
+	</div>
 	<pre bind:this={preRef} draggable={false}>
 		<NodeViewContent as="code" class={`language-${defaultLanguage}`} {...node.attrs} />	
 	</pre>
