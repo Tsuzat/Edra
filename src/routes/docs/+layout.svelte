aa612cbcd825d06d9e0dfa54b09042e8d4022434
Tsuzat <dev.tsuzat@gmail.com>
2025-03-20 16:57:28 +0530

    feat: enhance documentation layout with improved code block handling and new IFrame section

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -14,76 +14,78 @@
 	}
 
 	onMount(() => {
-		onMount(() => {
-			const preTags = document.querySelectorAll('pre.shiki');
-			preTags.forEach((tag) => {
-				// Tag has code inside
-				let fileName = '';
-				let codeTag = tag.querySelector('code');
-				if (codeTag) {
-					// Code tag has a property "data-file-name" we need to get it
-					fileName = codeTag.dataset.fileName || 'Terminal';
-				}
+		const preTags = document.querySelectorAll('pre.shiki');
+		preTags.forEach((tag) => {
+			// Tag has code inside
+			let fileName = '';
+			let codeTag = tag.querySelector('code');
+			if (codeTag) {
+				// Code tag has a property "data-file-name" we need to get it
+				fileName = codeTag.dataset.fileName || 'Terminal';
+			}
 
-				// Generate a new div
-				let preTagParent = tag.parentNode;
-				const div = document.createElement('div');
-				div.className = 'valkyrie rounded overflow-hidden';
+			// Generate a new div
+			let preTagParent = tag.parentNode;
+			const div = document.createElement('div');
+			div.className = 'valkyrie rounded overflow-hidden';
 
-				const divTopBar = document.createElement('div');
-				divTopBar.className =
-					'flex items-center justify-between px-4 rounded-t-md border-b border-muted-foreground/[10%] bg-muted/[25%] py-1.5 font-mono text-sm text-muted-foreground';
+			const divTopBar = document.createElement('div');
+			divTopBar.className =
+				'flex items-center justify-between px-4 rounded-t-md border-b border-muted-foreground/[10%] bg-muted/[25%] py-1.5 font-mono text-sm text-muted-foreground';
 
-				// Add one div with class "file name" and content as "filename.ext"
-				const fileNameDiv = document.createElement('div');
-				fileNameDiv.textContent = fileName;
+			// Add one div with class "file name" and content as "filename.ext"
+			const fileNameDiv = document.createElement('div');
+			fileNameDiv.textContent = fileName;
 
-				mount(CopyCode, {
-					target: divTopBar
-				});
-
-				divTopBar.prepend(fileNameDiv);
-
-				if (preTagParent) {
-					preTagParent.replaceChild(div, tag);
-					div.prepend(divTopBar);
-					div.appendChild(tag);
-				}
+			mount(CopyCode, {
+				target: divTopBar
 			});
 
-			// Add ID to all headings
-			const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
-			headings.forEach((heading) => {
-				heading.id = (heading.textContent?.replaceAll(' ', '-') ?? '').toLowerCase();
-				heading.classList.add('scroll-mt-16');
-			});
+			divTopBar.prepend(fileNameDiv);
 
-			// Custome Block Quote
-			const blockQuotes = document.querySelectorAll('blockquote');
-			blockQuotes.forEach((blockQuote) => {
-				const tagClass = blockQuote.firstChild?.textContent?.split(':')[0] || '';
-				if (tagClass === 'Info' || tagClass === 'Warning' || tagClass === 'Error') {
-					if (blockQuote.firstChild !== null) blockQuote.removeChild(blockQuote.firstChild);
-				}
+			if (preTagParent) {
+				preTagParent.replaceChild(div, tag);
+				div.prepend(divTopBar);
+				div.appendChild(tag);
+			}
+		});
 
-				switch (tagClass) {
-					case 'Warning':
-						blockQuote.classList.add(
-							'border-l-orange-500/80',
-							'bg-orange-500/10',
-							'text-orange-500'
-						);
-						break;
-					case 'Error':
-						blockQuote.classList.add('border-l-red-500/80', 'bg-red-500/10', 'text-red-500');
-						break;
-					case 'Info':
-						blockQuote.classList.add('border-l-green-500/50', 'bg-green-600/10', 'text-green-500');
-						break;
-					default:
-						break;
-				}
-			});
+		// Add ID to all headings
+		const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+		headings.forEach((heading) => {
+			heading.id = (heading.textContent?.replaceAll(' ', '-') ?? '').toLowerCase();
+			heading.classList.add('scroll-mt-16');
+		});
+
+		// Custome Block Quote
+		const blockQuotes = document.querySelectorAll('blockquote');
+		blockQuotes.forEach((blockQuote) => {
+			const tagClass = blockQuote.firstChild?.textContent?.split(':')[0] || '';
+			if (
+				tagClass === 'Info' ||
+				tagClass === 'Warning' ||
+				tagClass === 'Error' ||
+				tagClass === 'Note'
+			) {
+				if (blockQuote.firstChild !== null) blockQuote.removeChild(blockQuote.firstChild);
+			}
+
+			switch (tagClass) {
+				case 'Warning':
+					blockQuote.classList.add('border-l-orange-500/80', 'bg-orange-500/10', 'text-orange-500');
+					break;
+				case 'Error':
+					blockQuote.classList.add('border-l-red-500/80', 'bg-red-500/10', 'text-red-500');
+					break;
+				case 'Info':
+					blockQuote.classList.add('border-l-green-500/50', 'bg-green-600/10', 'text-green-500');
+					break;
+				case 'Note':
+					blockQuote.classList.add('border-l-blue-500/50', 'bg-blue-600/10', 'text-blue-500');
+					break;
+				default:
+					break;
+			}
 		});
 	});
 
@@ -104,6 +106,7 @@
 		{ name: 'Image Extended', href: '/docs#image-extended' },
 		{ name: 'Video Extended', href: '/docs#video-extended' },
 		{ name: 'Audio Extended', href: '/docs#audio-extended' },
+		{ name: 'IFrame Extended', href: '/docs#iframe-extended' },
 		{ name: 'Table', href: '/docs#table' },
 		{ name: 'Search And Replace', href: '/docs#search-and-replace' },
 		{ name: 'Links', href: '/docs#Links' },
