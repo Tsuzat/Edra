58a69fc65370b4677fa493d7e78c6223744ad9ea
Tsuzat <dev.tsuzat@gmail.com>
2025-03-24 22:36:10 +0530

    feat: add demo editor settings component with customizable toolbar and menu options

---
+++ src/lib/components/custom/demo-editor-settings.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { buttonVariants } from '../ui/button/button.svelte';
+	import { Checkbox } from '../ui/checkbox/index.js';
+	import * as Dialog from '../ui/dialog/index.js';
+	import Settings from 'lucide-svelte/icons/settings';
+	import { Label } from '../ui/label/index.js';
+
+	interface Props {
+		showToolBar: boolean;
+		showBubbleMenus: boolean;
+		editable: boolean;
+	}
+
+	let {
+		showToolBar = $bindable(true),
+		showBubbleMenus = $bindable(true),
+		editable = $bindable(true)
+	}: Props = $props();
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>
+		<Settings />
+		<span>Demo Settings</span>
+	</Dialog.Trigger>
+	<Dialog.Content class="h-fit max-h-[95vh] w-80 max-w-[95vw] p-4">
+		<Dialog.Header class="mb-4">
+			<Dialog.Title>Demo Edra Settings</Dialog.Title>
+		</Dialog.Header>
+		<div class="flex flex-col items-start gap-6">
+			<div class="flex w-full items-center gap-2">
+				<Checkbox id="toolbar" bind:checked={showToolBar} />
+				<Label for="toolbar" class="w-full cursor-pointer text-sm font-medium leading-none"
+					>Show Editor Toolbar</Label
+				>
+			</div>
+			<div class="flex w-full items-center gap-2">
+				<Checkbox id="menus" bind:checked={showBubbleMenus} />
+				<Label for="menus" class="w-full cursor-pointer text-sm font-medium leading-none"
+					>Show Editor Menus</Label
+				>
+			</div>
+			<div class="flex w-full items-center gap-2">
+				<Checkbox id="editable" bind:checked={editable} />
+				<Label for="editable" class="w-full cursor-pointer text-sm font-medium leading-none"
+					>Editable</Label
+				>
+			</div>
+		</div>
+	</Dialog.Content>
+</Dialog.Root>
