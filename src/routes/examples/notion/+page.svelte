b582b46ac5d5e5f09651f3dfdacc06ca7252993e
Tsuzat <dev.tsuzat@gmail.com>
2025-03-26 20:28:37 +0530

    feat: add Notion example page with editor and drag handle components

---
+++ src/routes/examples/notion/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { Edra } from '$lib/edra/shadcn/index.js';
+	import type { Content, Editor } from '@tiptap/core';
+	import defaultContent from '$lib/default_content.js';
+	import { Transaction } from '@tiptap/pm/state';
+	import DragHandle from '$lib/components/custom/notion/drag-handle.svelte';
+	import BubbleMenu from '$lib/components/custom/notion/bubble-menu.svelte';
+
+	// Editor states
+	let content = $state<Content>();
+	let editor = $state<Editor>();
+	let editable = $state(true);
+
+	$effect(() => {
+		localStorage.setItem('edra-content', JSON.stringify(content));
+	});
+
+	$effect(() => {
+		editor?.setEditable(editable);
+	});
+
+	if (browser) {
+		const rawDataString = localStorage.getItem('edra-content');
+
+		if (rawDataString === null) {
+			content = defaultContent;
+		} else {
+			const rawData: Content = JSON.parse(rawDataString);
+			content = rawData;
+		}
+	}
+
+	function onUpdate(props: { editor: Editor; transaction: Transaction }) {
+		content = props.editor.getJSON();
+	}
+</script>
+
+<div class="h-full w-full">
+	{#if editor && editable}
+		<DragHandle {editor} />
+		<BubbleMenu {editor} />
+	{/if}
+	<Edra
+		class="mx-auto h-full w-full max-w-3xl overflow-auto"
+		bind:editor
+		{editable}
+		{content}
+		{onUpdate}
+	/>
+</div>
+
+<style>
+	:global(.edra-editor) {
+		padding: 0 !important;
+	}
+</style>
