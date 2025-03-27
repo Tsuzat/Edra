fd794ae394a7033818787a9f6731687ef64a94ed
Tsuzat <dev.tsuzat@gmail.com>
2025-03-27 18:02:01 +0530

    feat: implement rate limiting for AI API calls to manage request frequency

--- src/routes/examples/notion/api/+server.ts
+++ src/routes/examples/notion/api/+server.ts
@@ -1,10 +1,23 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { fixGrammer, makeItLonger, makeItShorter, reWrite } from './ai.js';
+import { RateLimiter } from 'sveltekit-rate-limiter/server';
 
 interface ReqBody {
 	tool: 'longer' | 'shorter' | 're-write' | 'fix-grammar';
 	text: string;
+	apiKey?: string;
 }
+
+const limiter = new RateLimiter({
+	IP: [
+		[100, 'd'],
+		[20, 'm']
+	], // Limit to 100 requests per hour per IP address
+	IPUA: [
+		[100, 'd'],
+		[20, 'm']
+	] // Limit to 50 requests per minute per IP + User Agent
+});
 
 async function handleAICall(type: 'longer' | 'shorter' | 're-write' | 'fix-grammar', text: string) {
 	switch (type) {
@@ -21,9 +34,21 @@ async function handleAICall(type: 'longer' | 'shorter' | 're-write' | 'fix-gramm
 	}
 }
 
-export const POST: RequestHandler = async ({ request }) => {
+export const POST: RequestHandler = async (event) => {
+	if (await limiter.isLimited(event)) {
+		return json(
+			{
+				message:
+					'Calm down, mate. Maximum 100 requests per day and 20 requests per minute is allowed.'
+			},
+			{ status: 429 }
+		);
+	}
+
 	try {
+		const request = event.request;
 		const data: ReqBody = await request.json();
+
 		const { tool, text } = data;
 		if (tool === undefined || text === undefined || text.trim() === '') {
 			return json({ message: 'Please provide a tool and text' }, { status: 400 });
