cb4d55c38b3304fc3bbc2a2f8c429d053619779f
Tsuzat <dev.tsuzat@gmail.com>
2025-03-27 14:59:03 +0530

    feat: add API endpoint for AI text manipulation tools

---
+++ src/routes/examples/notion/api/+server.ts
@@ -0,0 +1,37 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+import { fixGrammer, makeItLonger, makeItShorter, reWrite } from './ai.js';
+
+interface ReqBody {
+	tool: 'longer' | 'shorter' | 're-write' | 'fix-grammar';
+	text: string;
+}
+
+async function handleAICall(type: 'longer' | 'shorter' | 're-write' | 'fix-grammar', text: string) {
+	switch (type) {
+		case 'longer':
+			return makeItLonger(text);
+		case 'shorter':
+			return makeItShorter(text);
+		case 're-write':
+			return reWrite(text);
+		case 'fix-grammar':
+			return fixGrammer(text);
+		default:
+			return fixGrammer(text);
+	}
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+	try {
+		const data: ReqBody = await request.json();
+		const { tool, text } = data;
+		if (tool === undefined || text === undefined || text.trim() === '') {
+			return json({ error: 'Please provide a tool and text' }, { status: 400 });
+		}
+		const aiRes = await handleAICall(tool, text);
+		return json({ message: aiRes }, { status: 200 });
+	} catch (error) {
+		console.error('Error parsing JSON:', error);
+		return json({ error: error }, { status: 400 });
+	}
+};
