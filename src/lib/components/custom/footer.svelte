cf6dc608941cb812e0bbba21f9eb9d64802aa9d1
Tsuzat <dev.tsuzat@gmail.com>
2025-03-27 22:48:21 +0530

    fix: update footer component to include Examples link and remove License link

--- src/lib/components/custom/footer.svelte
+++ src/lib/components/custom/footer.svelte
@@ -3,11 +3,10 @@
 
 <footer class="mt-2 flex h-fit items-center justify-center gap-2 p-1 text-sm">
 	<a href="/docs" class="underline">Docs</a> •
+	<a href="/docs" class="underline">Examples</a> •
 	<a href="https://github.com/Tsuzat/Edra" target="_blank" class="underline">Github</a> •
 	<span
 		>Copyright © 2025
 		<a href="https://tsuzat.com" target="_blank" class="underline">Tsuzat</a>
 	</span>
-	•
-	<a href="/LICENSE" class="underline">License</a>
 </footer>
