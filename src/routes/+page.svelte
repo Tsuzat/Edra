bdc80312dffcb251f500c7cc483c43da73aed04f
Tsuzat <dev.tsuzat@gmail.com>
2025-05-21 23:43:54 +0530

    feat: restructure page layout to include editor toolbar and binding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import EdraEditor from '$lib/edra/shadcn/editor.svelte';
 	import Toolbar from '$lib/edra/shadcn/toolbar.svelte';
+	import { Editor } from '@tiptap/core';
+	let editor = $state<Editor>();
 </script>
 
-<EdraEditor />
+<main class="mx-auto my-auto flex max-w-4xl flex-col items-center justify-center">
+	<div class="mt-12 size-full rounded-md border border-dashed">
+		{#if editor}
+			<Toolbar class="w-full border-b border-dashed" {editor} />
+		{/if}
+		<EdraEditor bind:editor class="h-80" />
+	</div>
+</main>
