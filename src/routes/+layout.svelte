c58706ac90787f5002084f386b1e272171c2dd25
Tsuzat <dev.tsuzat@gmail.com>
2025-05-21 17:25:28 +0530

    feat: update layout and page components to include ModeWatcher and EdraEditor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import Header from '$lib/components/custom/Header.svelte';
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+
+<Header />
 {@render children()}
