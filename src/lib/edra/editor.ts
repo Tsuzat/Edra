8641e04d8daca9875c6a20c2965dda6c5f0da6db
Tsuzat <dev.tsuzat@gmail.com>
2025-05-22 17:25:37 +0530

    feat: add configuration for ordered and bullet lists in editor

--- src/lib/edra/editor.ts
+++ src/lib/edra/editor.ts
@@ -1,6 +1,13 @@
 import { Editor, Extension, type EditorOptions } from '@tiptap/core';
 import StarterKit from '@tiptap/starter-kit';
 import { getHandlePaste } from './utils.js';
+import Subscript from '@tiptap/extension-subscript';
+import Superscript from '@tiptap/extension-superscript';
+import Typography from '@tiptap/extension-typography';
+import { ColorHighlighter } from './extensions/ColorHighlighter.js';
+import { SmilieReplacer } from './extensions/SmilieReplacer.js';
+import { FontSize, TextStyle } from '@tiptap/extension-text-style';
+import TextAlign from '@tiptap/extension-text-align';
 
 export default (
 	element?: HTMLElement,
@@ -11,9 +18,35 @@ export default (
 		element,
 		extensions: [
 			StarterKit.configure({
+				orderedList: {
+					HTMLAttributes: {
+						class: 'list-decimal'
+					}
+				},
+				bulletList: {
+					HTMLAttributes: {
+						class: 'list-disc'
+					}
+				},
 				heading: {
 					levels: [1, 2, 3, 4]
-				}
+				},
+				link: {
+					openOnClick: false,
+					autolink: true,
+					linkOnPaste: true
+				},
+				codeBlock: false
+			}),
+			Subscript,
+			Superscript,
+			Typography,
+			ColorHighlighter,
+			TextStyle,
+			FontSize,
+			SmilieReplacer,
+			TextAlign.configure({
+				types: ['heading', 'paragraph']
 			}),
 			...(extensions ?? [])
 		],
