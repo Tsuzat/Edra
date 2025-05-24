d337a56a6062cdf0c907dc4d3c8790d7ff50b3e7
Tsuzat <dev.tsuzat@gmail.com>
2025-05-24 16:15:48 +0530

    Persist editor content to local storage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
 	import EdraEditor from '$lib/edra/shadcn/editor.svelte';
 	import Toolbar from '$lib/edra/shadcn/toolbar.svelte';
-	import { Editor } from '@tiptap/core';
+	import { Editor, type Content } from '@tiptap/core';
+	import { onMount } from 'svelte';
 	let editor = $state<Editor>();
+	let content = $state<Content>();
+
+	onMount(() => {
+		const rawContent = localStorage.getItem('edra-content');
+		if (rawContent) {
+			content = JSON.parse(rawContent);
+		}
+	});
+
+	$effect(() => {
+		console.log('[DEBUG]', $state.snapshot(content));
+		localStorage.setItem('edra-content', JSON.stringify(content));
+	});
+
+	function onUpdate() {
+		content = editor?.getJSON();
+	}
 </script>
 
 <main class="mx-auto my-auto flex max-w-4xl flex-col items-center justify-center">
 	<div class="mt-12 size-full rounded-md border border-dashed">
 		{#if editor}
-			<Toolbar class="w-full border-b border-dashed" {editor} />
+			<Toolbar class="flex w-full items-center overflow-x-scroll border-b border-dashed" {editor} />
 		{/if}
-		<EdraEditor bind:editor class="h-80" />
+		<EdraEditor bind:editor {content} class="h-96" {onUpdate} />
 	</div>
 </main>
