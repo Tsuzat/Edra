88ed0ed3cae376b89d1720c8cbf516c58b0d69ed
Tsuzat <dev.tsuzat@gmail.com>
2025-05-24 19:06:32 +0530

    Add MediaPlaceHolder Svelte component

---
+++ src/lib/edra/components/MediaPlaceHolder.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { Icon } from '@lucide/svelte';
+	import type { Snippet } from 'svelte';
+	import { NodeViewWrapper } from 'svelte-tiptap';
+
+	interface Props {
+		icon?: typeof Icon;
+		title?: string;
+		onClick?: () => void;
+		class?: string;
+		children?: Snippet<[]>;
+	}
+
+	const { icon, title, onClick, class: className = '', children }: Props = $props();
+</script>
+
+<NodeViewWrapper
+	as="div"
+	contenteditable="false"
+	class={`media-placeholder ${className}`}
+	onclick={onClick}
+>
+	{#if !children && icon && title}
+		{@const Icon = icon}
+		<Icon />
+		<div>{title}</div>
+	{/if}
+	{@render children?.()}
+</NodeViewWrapper>
