cbcdb8c18abb2b004b67d7d7ebda2a72693f1ff3
Tsuzat <dev.tsuzat@gmail.com>
2025-05-27 23:40:03 +0530

    Add SlashCommand integration to editor for enhanced command functionality

--- src/lib/edra/shadcn/editor.svelte
+++ src/lib/edra/shadcn/editor.svelte
@@ -30,6 +30,8 @@
 	import TableCol from './menus/TableCol.svelte';
 	import TableRow from './menus/TableRow.svelte';
 	import Link from './menus/Link.svelte';
+	import slashcommand from '../extensions/slash-command/slashcommand.js';
+	import SlashCommandList from './components/SlashCommandList.svelte';
 
 	const lowlight = createLowlight(all);
 
@@ -64,7 +66,8 @@
 				AudioPlaceholder(AudioPlaceHolderComp),
 				AudioExtended(AudioExtendedComp),
 				IFramePlaceholder(IFramePlaceHolderComp),
-				IFrameExtended(IFrameExtendedComp)
+				IFrameExtended(IFrameExtendedComp),
+				slashcommand(SlashCommandList)
 			],
 			{
 				onUpdate,
