9649cbb80c2413bbae01c156471b34c26c52f76c
Tsuzat <dev.tsuzat@gmail.com>
2025-05-28 08:56:25 +0530

    Fix import path for EdraToolBarCommands in ToolBarIcon component

--- src/lib/edra/shadcn/components/ToolBarIcon.svelte
+++ src/lib/edra/shadcn/components/ToolBarIcon.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
-	import type { EdraToolBarCommands } from '$lib/edra/commands/types.js';
+	import type { EdraToolBarCommands } from '../../commands/types.js';
 	import type { Editor } from '@tiptap/core';
 	import EdraToolTip from './EdraToolTip.svelte';
 	import { cn } from '$lib/utils.js';
