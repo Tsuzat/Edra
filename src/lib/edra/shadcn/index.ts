91510794649465423b6861da12a85fe3193fe3b9
Tsuzat <dev.tsuzat@gmail.com>
2025-05-31 13:16:31 +0530

    Export DragHandleExtended and replace DragHandle usage

--- src/lib/edra/shadcn/index.ts
+++ src/lib/edra/shadcn/index.ts
@@ -1,3 +1,4 @@
 export { default as EdraEditor } from './editor.svelte';
 export { default as EdraToolBar } from './toolbar.svelte';
 export { default as BubbleMenu } from './menus/Menu.svelte';
+export { default as DragHandleExtended } from './drag-handle-extended.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,7 @@
 	import { browser } from '$app/environment';
 	import Particles from '$lib/components/custom/Particles.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import DragHandle from '$lib/edra/components/DragHandle.svelte';
-	import { EdraEditor, EdraToolBar } from '$lib/edra/shadcn/index.js';
+	import { EdraEditor, EdraToolBar, DragHandleExtended } from '$lib/edra/shadcn/index.js';
 	import { Editor, type Content } from '@tiptap/core';
 	let editor = $state<Editor>();
 	let content = $state<Content>();
@@ -64,7 +63,7 @@
 				class="bg-secondary/50 flex w-full items-center overflow-x-scroll border-b border-dashed p-0.5"
 				{editor}
 			/>
-			<DragHandle {editor} />
+			<DragHandleExtended {editor} />
 		{/if}
 		<EdraEditor
 			bind:editor
