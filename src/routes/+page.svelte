e69e2b03685218be17f71659b667b3c70b0c8b01
Tsuzat <dev.tsuzat@gmail.com>
2025-06-02 11:47:11 +0530

    Fix import and usage of EdraDragHandleExtended for consistency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { browser } from '$app/environment';
 	import Particles from '$lib/components/custom/Particles.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { EdraEditor, EdraToolBar, DragHandleExtended } from '$lib/edra/shadcn/index.js';
+	import { EdraEditor, EdraToolBar, EdraDragHandleExtended } from '$lib/edra/shadcn/index.js';
 	import { Editor, type Content } from '@tiptap/core';
 	let editor = $state<Editor>();
 	let content = $state<Content>();
@@ -60,10 +60,10 @@
 	<div class="bg-background z-50 mt-12 size-full max-w-5xl rounded-md border border-dashed">
 		{#if editor && !editor.isDestroyed}
 			<EdraToolBar
-				class="bg-secondary/50 flex w-full items-center overflow-x-scroll border-b border-dashed p-0.5"
+				class="bg-secondary/50 flex w-full items-center overflow-x-auto border-b border-dashed p-0.5"
 				{editor}
 			/>
-			<DragHandleExtended {editor} />
+			<EdraDragHandleExtended {editor} />
 		{/if}
 		<EdraEditor
 			bind:editor
