dbff229b7f0488b46fcad5b5490c306af776ee61
Tsuzat <dev.tsuzat@gmail.com>
2025-06-02 20:14:43 +0530

    fix: add Vercel analytics and speed insights integrations

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,7 @@
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+injectSpeedInsights();
+injectAnalytics();
+
 export const prerender = true;
 export const ssr = false;
