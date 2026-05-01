6c9e312384b46ec0a5edc7445ec4814c8624833d
Max Farrell <maxffarrell@gmail.com>
2026-04-30 22:55:24 -0500

    footer year

--- src/lib/components/custom/Footer.svelte
+++ src/lib/components/custom/Footer.svelte
@@ -4,7 +4,7 @@
 <footer class="text-muted-foreground my-4 flex flex-wrap items-center justify-center gap-2 text-sm">
 	<a href="/docs">Docs</a> •
 	<a href="/examples">Examples</a>•
-	<p>© 2025 <a href="https://tsuzat.com">Tsuzat</a></p>
+	<p>© 2026 <a href="https://tsuzat.com">Tsuzat</a></p>
 </footer>
 
 <style>
